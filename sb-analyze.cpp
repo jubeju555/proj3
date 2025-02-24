4617b0afd191f151899e495692f1610bea649adc
judah <judahjosh@gmail.com>
2025-02-24 15:56:24 -0500

    10 gradescripts

--- sb-analyze.cpp
+++ sb-analyze.cpp
@@ -182,7 +182,7 @@ int main(int argc, char **argv)
     if (data.size >= s->mss && data.has_goal && data.size > 1)
     {
       int Grow = root / s->column + 1;
-      int Gcol = root % s->column - 1;
+      int Gcol = root % s->column + 1;
       char scolor = s->board[root];
       printf("  Size: %2d  Char: %c  Scoring Cell: %d,%d\n", data.size, scolor, Grow, Gcol);
     }
