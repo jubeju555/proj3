4966edb8e2a3c6a2fc77f5d7583360c5a8567c47
judah <judahjosh@gmail.com>
2025-02-25 17:01:21 -0500

    swap function written

--- sb-play.cpp
+++ sb-play.cpp
@@ -3,6 +3,34 @@
 
 using namespace std;
 
+class Superball {
+  public:
+    Superball(int argc, char **argv);
+    int r;
+    int c;
+    int mss;
+    int empty;
+    vector <int> board;
+    vector <int> goals;
+    vector <int> colors;
+};
+
+// void usage(const char *s) 
+// {
+//   fprintf(stderr, "usage: sb-read rows cols min-score-size colors\n");
+//   if (s != NULL) fprintf(stderr, "%s\n", s);
+  
+//   exit(1);
+// }
+ 
+int swap(int &a, int &b) {
+  int temp = a;
+  a = b;
+  b = temp;
+  return 0;
+}
+
+
 int main() {
 
   cout << "This program doesn't do anything yet.\n";
